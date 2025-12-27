c2571b5575945d67a0873dd3bad6f80f92fe4c75
ZoqkMaze <155395129+ZoqkMaze@users.noreply.github.com>
2025-12-27 23:08:13 +0100

    finish refector (untested)

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 import random
 from enum import Enum
 
+# todo: add error codes for each class
+
 
 class LobbyState(Enum):
     JOIN = "join"
@@ -11,13 +13,11 @@ class LobbyState(Enum):
 
 class GameState(Enum):
     DEALING = "dealing"
-    SWAPING = "swaping"
     PLAYING = "playing"
     EVALUATING = "evaluating"
     def next(self):
         transition = {
-            GameState.DEALING: GameState.SWAPING,
-            GameState.SWAPING: GameState.PLAYING,
+            GameState.DEALING: GameState.PLAYING,
             GameState.PLAYING: GameState.EVALUATING,
             GameState.EVALUATING: GameState.DEALING,
         }
@@ -35,9 +35,9 @@ class Color(Enum):
 class Flag(Enum):
     RED_FLAG = "double normal points"
     BLUE_FLAG = "clear all points"
-    YELLOW_FLAG = -5
-    LOW_FLAG = 5
-    HIGH_FLAG = 10
+    YELLOW_FLAG = "minus 5 points"
+    LOW_FLAG = "plus 5 points"
+    HIGH_FLAG = "plus 5 points"
     NONE_FLAG = "no effect"
 
 
@@ -48,74 +48,122 @@ class Spell(Enum):
     HIGH_SPELL = 30
 
 
-class Game:
-    # Zahlen 1 bis 13 in den Farben rot, blau, grün und gelb
-    # zusätzlich 4 Narren
-    # statt ABC einer logger klasse übergeben, die aufgerufen wird, wenn infos übermittelt werden
+class Card:
 
-    # todo: change constants to enum
-    RED = "red"
-    BLUE = "blue"
-    GREEN = "green"
-    YELLOW = "yellow"
-    BLANCK = "blanck"
+    MIN_RANK = 0  # inclusive
+    MAX_RANK = 15  # exclusiv
 
-    COLORS = [RED, BLUE, GREEN, YELLOW, BLANCK]
-    TRUE_COLORS = [RED, BLUE, GREEN, YELLOW]
+    WITCH_RANK = 11
+    QUEEN_RANK = 12
 
-    RED_FLAG = "double normal points"
-    BLUE_FLAG = "clear all points"
-    YELLOW_FLAG = "minus 5"
-    LOW_FLAG = "plus 5"
-    HIGH_FLAG = "plus 10"
-    NONE_FLAG = "no effect"
+    def __init__(self, color, rank):
+        self.__changeable = True
+        self.__color = None
+        self.__rank = None
 
-    MIN_RANK = 0  # inklusive
-    MAX_RANK = 15  # exklusive
+        if self.set_color(color) or self.set_rank(rank):
+            raise KeyError()
 
-    MIN_POINTS = 0  # inklusive
-    MAX_DOUBLE_POINTS = 15
-    HIGH_POINTS = 10
-    LOW_POINTS = 5
+        self.__changeable = False
+    
+    def __gt__(self, other):
+        if isinstance(other, Card):
+            return self.__rank > other.rank
+        raise ValueError()
+    
+    def __repr__(self):
+        return f"Card<{self.__color}-{self.__rank}>"
+    
+    @property
+    def witch(self):
+        return self.__rank == Card.WITCH_RANK
+    
+    @property
+    def queen(self):
+        return self.__rank == Card.QUEEN_RANK
+    
+    @property
+    def color(self):
+        return self.__color
+    
+    @property
+    def rank(self):
+        return self.__rank
+    
+    @property
+    def points(self):
+        return 1 if self.__color is Color.RED and not self.witch else 0
+    
+    @property
+    def flag(self):
+        if self.__color is Color.GREEN and self.queen: return Flag.HIGH_FLAG
+        if self.witch:
+            match self.__color:
+                case Color.RED:    return Flag.RED_FLAG
+                case Color.BLUE:   return Flag.BLUE_FLAG
+                case Color.GREEN:  return Flag.LOW_FLAG
+                case Color.YELLOW: return Flag.YELLOW_FLAG
+                case _: return Flag.NONE_FLAG
+        return Flag.NONE_FLAG
+    
+    def set_color(self, color: Color):
+        if self.__changeable:
+            if color in Color:
+                self.__color = color
+                return 0
+        return 1
+    
+    def set_rank(self, rank):
+        if self.__changeable:
+            if Card.MIN_RANK <= rank < Card.MAX_RANK:
+                self.__rank = rank
+                return 0
+        return 1
 
-    NO_SPELL = 0
-    LOW_SPELL = 20
-    MEDIUM_SPELL = 25
-    HIGH_SPELL = 30
+
+class Game:  # todo: finish class
 
     MIN_PLAYER = 3  # inclusiv
     MAX_PLAYER = 6  # inclusiv
+        
+    ALL_CARDS = [
+        Card(c, r)
+        for c in Color
+        for r in range(Card.MIN_RANK+1, Card.MAX_RANK)
+        if c is not Color.BLANCK
+    ] + [Card(Color.BLANCK, Card.MIN_RANK) for _ in range(len(Color))]
 
 
     def __init__(self, player_count):
         self.__player_count = player_count
         if not Game.MIN_PLAYER <= player_count <= Game.MAX_PLAYER: raise ValueError("wrong player number")
-        self.__cards = list(Card(c, r) for c in Game.TRUE_COLORS for r in range(Game.MIN_RANK+1, Game.MAX_RANK)) + list(Card(Game.BLANCK, Game.MIN_RANK) for _ in range(len(Game.TRUE_COLORS)))
         self.__current_stitch: Stitch = Stitch(self.__player_count)
         self.__round = 0
-        # self.__total_stitches = len(self.__cards) // self.__player_count  # todo: remove as well
         self.__state = GameState.DEALING
     
     def next_state(self):
         self.__state = self.__state.next()
+    
+    @property
+    def total_stitches(self):
+        return len(Game.ALL_CARDS) // self.__player_count
 
-    def game_status(self):
+    @property
+    def status(self):
         return {
             "typ": "game",
             "player_count": self.__player_count,
-            "status": self.__state,
+            "state": self.__state,
             "round": self.__round,
+            "last_round": self.__total_stitches,
         }
-
-    @property
-    def cards(self):
-        return self.__cards
     
     @property
     def current_color(self):
-        return self.__current_stitch.get_color()
+        return self.__current_stitch.color
 
-    def get_player_count(self):
+    @property
+    def player_count(self):
         return self.__player_count
     
     def play_card(self, card):
@@ -132,22 +180,25 @@ class Game:
         return 0
     
     def new_stitch(self):
-        if not self.__current_stitch.is_full():  # includes Game.PLAYING
+        if not self.__current_stitch.full:
             return 1
         old_stitch = self.__current_stitch
         self.__current_stitch = Stitch(self.__player_count)
         self.__round += 1
+        if self.__round > self.total_stitches:
+            self.__round = 0
+            self.next_state()
         """determine winner, next player, give stitch to player"""
         return old_stitch  # kann ausgewertet und dem gewinner gegeben werden
 
     def deal_cards(self):
         if self.__state is not GameState.DEALING:
             return 1
-        if len(self.__cards)%self.__player_count: raise ValueError("wrong number of players")
+        if len(Game.ALL_CARDS) % self.__player_count: raise ValueError("wrong number of players")
         stacks = [[] for _ in range(self.__player_count)]
-        card_per_player = len(self.__cards) / self.__player_count
+        card_per_player = len(Game.ALL_CARDS) / self.__player_count
         open_p = [x for x in range(self.__player_count)]
-        for c in self.__cards:
+        for c in Game.ALL_CARDS:
             p = random.randint(0, len(open_p)-1)
             stacks[open_p[p]].append(c)
             if len(stacks[open_p[p]]) >= card_per_player:
@@ -155,98 +206,23 @@ class Game:
         self.next_state()
         return stacks
     
-    def swap_cards(self):
-        if self.__state is not Game.SWAPING:
-            return 1
-        
-        # todo
-        self.next_state()
-        return 0
-    
     def evaluate(self):
-        if self.__state is not Game.EVALUATING:
+        if self.__state is not GameState.EVALUATING:
             return 1
+        
+        # todo
+
         self.next_state()
         return 0
         
         # todo
-        spell = max([p.has_spell() for p in self.__players])
+        spell = max([p.spell for p in self.__players])
         if spell:
             for p in self.__players:
-                if not p.has_spell(): p.add_game_score(spell)
+                if not p.spell: p.add_game_score(spell)
         else:
             for p in self.__players:
-                p.add_game_score(p.get_score())
-
-
-
-class Card:
-
-    def __init__(self, color, rank):
-        # todo: finish  initialiser
-        self.__changeable = True
-        self.__color = None  # Farbe der Karte (einschließlich Narr)
-        self.__rank = None  # Zahl der Kare 0..13 (einschließlich Narr)
-
-        if self.set_color(color) or self.set_rank(rank):
-            raise KeyError()
-
-        self.__changeable = False
-    
-    def __gt__(self, other):
-        if isinstance(other, Card):
-            return self.__rank > other.get_rank()
-        return False
-    
-    def __repr__(self):
-        return f"Card<{self.__color}-{self.__rank}>"
-    
-    @property
-    def color(self):
-        return self.__color
-    
-    @property
-    def rank(self):
-        return self.__rank
-    
-    def get_color(self):
-        # todo: remove
-        return self.__color
-    
-    def is_color(self, color):
-        return self.__color is color
-    
-    def set_color(self, color):
-        if self.__changeable:
-            if color in Color:
-                self.__color = color
-                return
-        return 1  # Fehler
-
-    def get_rank(self):
-        # todo: remove
-        return self.__rank
-    
-    def set_rank(self, rank):
-        if self.__changeable:
-            if Game.MIN_RANK <= rank < Game.MAX_RANK:
-                self.__rank = rank
-                return
-        return 1  # Fehler
-    
-    def points(self):
-        return 1 if self.is_color(Game.RED) and self.__rank != 11 else 0
-    
-    def flag(self):
-        if self.is_color(Game.GREEN) and self.__rank == 12: return Game.HIGH_FLAG
-        if self.__rank == 11:
-            match self.__color:
-                case Game.RED: return Game.RED_FLAG
-                case Game.BLUE: return Game.BLUE_FLAG
-                case Game.GREEN: return Game.LOW_FLAG
-                case Game.YELLOW: return Game.YELLOW_FLAG
-                case _: return Game.NONE_FLAG
-        return Game.NONE_FLAG
+                p.add_game_score(p.score)
     
 
 class Stitch:
@@ -256,51 +232,63 @@ class Stitch:
 
     def __init__(self, player_count):
         self.__player_count = player_count
+        self.__next_index = 0
         self.__cards: list[Card] = []
-        self.__color = Game.BLANCK
+        self.__color = Color.BLANCK
         self.__points = 0
         self.__flags = []
         self.__red_cards = 0
+        self.__winner = -1
 
-    def get_red_cards(self):
+    @property
+    def red_cards(self):
         return self.__red_cards
     
-    def get_color(self):
+    @property
+    def color(self):
         return self.__color
     
-    def get_points(self):
+    @property
+    def points(self):
         return self.__points
     
-    def get_flags(self):
+    @property
+    def flags(self):
         return self.__flags
     
-    def is_full(self):
+    @property
+    def full(self):
         return len(self.__cards) == self.__player_count
     
-    def play_card(self, card):
-        if self.is_full():
-            return Stitch.FULL_ERROR
-        if isinstance(card, Card):
-            if self.__color == Game.BLANCK: self.__color = card.get_color()
-            self.__cards.append(card)
-            self.__points += card.points()
-            flag = card.flag()
-            if flag != Game.NONE_FLAG: self.__flags.append(flag)
-            if card.is_color(Game.RED): self.__red_cards += 1
-            return
-        return Stitch.NO_CARD_ERROR
+    @property
+    def winner(self):
+        return self.__winner
     
-    def evaluate(self):
-        if self.__player_count == len(self.__cards):
-            win_index = 0
-            for i, c in enumerate(self.__cards):
-                if c.is_color(self.__color) and c > self.__cards[win_index]:
-                    win_index = i
-            return win_index
-        return -1
+    def play_card(self, card):
+        if self.full:
+            return Stitch.FULL_ERROR
+        if not isinstance(card, Card):
+            return Stitch.NO_CARD_ERROR
+        
+        if self.__color == Color.BLANCK: self.__color = card.color
+        if self.__winner == -1: self.__winner = 0
+        if card.color is self.__color and card > self.__cards[self.__winner]: self.__winner = self.__next_index
+        self.__next_index += 1
+        self.__cards.append(card)
+        self.__points += card.points
+        flag = card.flag
+        if flag != Flag.NONE_FLAG: self.__flags.append(flag)
+        if card.__color is Color.RED: self.__red_cards += 1
+        return
     
 
 class Player:
+
+    MIN_POINTS = 0  # inklusive
+    MAX_DOUBLE_POINTS = 15
+    HIGH_POINTS = 10
+    LOW_POINTS = 5
+
     def __init__(self, id):
         self.__id = id
         self.__cards: dict[str, Card] = {}
@@ -309,7 +297,7 @@ class Player:
         self.__flags = []
         self.__red_cards = 0
         self.__game_score = 0  # Punkte über alle Spielrunden
-        self.__card_id = 0
+        self.__next_card_id = 0
     
     @property
     def id(self):
@@ -317,56 +305,60 @@ class Player:
     
     @property
     def cards(self):
-        return list(self.__cards.values)
+        return self.__cards
     
     @property
     def flags(self):
         return self.__flags
 
-    def get_game_score(self):
+    @property
+    def number_of_cards(self):
+        return len(self.__cards)
+
+    @property
+    def game_score(self):
         return self.__game_score
-    
+
+    @property
+    def spell(self):
+        if self.__red_cards == Card.MAX_RANK - Card.MIN_RANK - 2:
+            if Flag.LOW_FLAG in self.__flags:
+                if Flag.HIGH_FLAG in self.__flags:
+                    return Spell.HIGH_SPELL
+                else:
+                    return Spell.LOW_SPELL
+            if Flag.HIGH_FLAG in self.__flags:
+                return Spell.MEDIUM_SPELL
+        return Spell.NO_SPELL
+
+    @property
+    def score(self):
+        score = min(Player.MAX_DOUBLE_POINTS, 2*self.__points) if Flag.RED_FLAG in self.__flags else self.__points
+        if Flag.LOW_FLAG in self.__flags: score += Player.LOW_POINTS
+        if Flag.HIGH_FLAG in self.__flags: score += Player.HIGH_POINTS
+        if Flag.YELLOW_FLAG in self.__flags: score -= Player.LOW_POINTS
+        if Flag.BLUE_FLAG in self.__flags: score = 0
+        return max(Player.MIN_POINTS, score)
+
     def add_game_score(self, points):
         self.__game_score += points
 
-    def has_spell(self):
-        if self.__red_cards == Game.MAX_RANK - Game.MIN_RANK - 2:
-            if Game.LOW_FLAG in self.__flags:
-                if Game.HIGH_FLAG in self.__flags:
-                    return Game.HIGH_SPELL
-                else:
-                    return Game.LOW_SPELL
-            if Game.HIGH_FLAG in self.__flags:
-                return Game.MEDIUM_SPELL
-        return Game.NO_SPELL
-
-    def get_score(self):
-        score = min(Game.MAX_DOUBLE_POINTS, 2*self.__points) if Game.RED_FLAG in self.__flags else self.__points
-        if Game.LOW_FLAG in self.__flags: score += Game.LOW_POINTS
-        if Game.HIGH_FLAG in self.__flags: score += Game.HIGH_POINTS
-        if Game.YELLOW_FLAG in self.__flags: score = max(0, score-Game.LOW_POINTS)
-        if Game.BLUE_FLAG in self.__flags: score = 0
-        return score
-
     def has_card(self, card):
-        return card in self.__cards.values
+        return card in self.__cards.values()
     
     def has_card_id(self, card_id):
         return card_id in self.__cards
     
-    def number_of_cards(self):
-        return len(self.__cards)
-    
     def has_color(self, color):
-        for c in self.__cards:
-            if c.is_color(color): return True
+        for c in self.__cards.values():
+            if c.color is color: return True
         return False
 
     def get_stitch(self, stitch: Stitch):
         self.__stitches.append(stitch)
-        self.__points += stitch.get_points()
-        self.__flags += stitch.get_flags()
-        self.__red_cards += stitch.get_red_cards()
+        self.__points += stitch.points
+        self.__flags += stitch.flags
+        self.__red_cards += stitch.red_cards
 
     def play_card(self, card_id):
         return self.__cards.pop(card_id)
@@ -376,12 +368,12 @@ class Player:
     
     def add_cards(self, cards):
         for c in cards:
-            self.__cards[f"c{self.__card_id}"] = c
-            self.__card_id += 1
+            self.__cards[f"c{self.__next_card_id}"] = c
+            self.__next_card_id += 1
     
     def add_card(self, card):
-        self.__cards[f"c{self.__card_id}"] = card
-        self.__card_id += 1
+        self.__cards[f"c{self.__next_card_id}"] = card
+        self.__next_card_id += 1
     
     def remove_card_id(self, card_id):
         del self.__cards[card_id]
@@ -392,28 +384,29 @@ class Player:
         self.__points = 0
         self.__flags = []
         self.__red_cards = 0
-        self.__card_id = 0
+        self.__next_card_id = 0
 
 
 class GameManager:
     def __init__(self):
-        self.__players: dict[str, Player] = {}
-        self.__player_ids = []
+        self.__players: list[Player] = []
         self.__game: Game = None
         self.__current_player = 0
         self.__state = LobbyState.JOIN
     
+    @property
+    def player_ids(self):
+        return [p.id for p in self.__players]
+    
     def join(self, player: Player):
         if self.__state is LobbyState.JOIN:
-            self.__players[player.id] = player
-            self.__player_ids.append(player.id)
+            self.__players.append(player)
             return 0
         return 1
     
     def leave(self, player_id):
         if self.__state is LobbyState.JOIN:
-            del self.__players[player_id]
-            self.__player_ids.remove(player_id)
+            self.__players.remove(player_id)
             return
         self.__state = LobbyState.CANCEL
         # todo: handle break
@@ -423,13 +416,13 @@ class GameManager:
         if self.__state is not LobbyState.GAME:
             return 1
         
-        if player_id not in self.__player_ids:
+        if player_id not in self.player_ids:
             return 1
         
-        if not self.__current_player == self.__player_ids.index(player_id):
+        if not self.__current_player == self.player_ids.index(player_id):
             return 1
         
-        player = self.__players[player_id]
+        player = self.__players[self.__current_player]
         
         if not player.has_card_id(card_id):
             return 1
@@ -437,7 +430,7 @@ class GameManager:
         card = player.get_card(card_id)
         color = self.__game.current_color
 
-        if color is not Color.BLANCK and card.get_color() is not Color.BLANCK:
+        if color is not Color.BLANCK and card.color is not Color.BLANCK:
             # apply zugzwang
             if player.has_color(color) and card.color is not color:
                 return 1
